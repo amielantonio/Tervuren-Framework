e6d7fef4aa00715210c4ca8ff7f5ded3ff1db5d7
Amiel Antonio <amiel.cornerstone@gmail.com>
2018-09-27 15:55:15 +0800

    Add PostType Class

---
+++ lib/Database/PostType.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shore360
+ * Date: 27/09/2018
+ * Time: 3:48 PM
+ */
+
+namespace App\Database;
+
+class PostType
+{
+
+    protected $name;
+
+    protected $args = [];
+
+    protected $labels;
+
+    public function __construct()
+    {
+
+    }
+
+
+    
+
+}
